cb94f5e5d8130b5d56fa2abbf95d5614ad8c735a
Justin Almas <justinaalmas@gmail.com>
2024-12-24 23:29:57 -0500

    add to list

--- src/malloc.s
+++ src/malloc.s
@@ -1,13 +1,19 @@
 .global _start
 
-.section .data
+.data
     exit_message: .asciz "Exiting\n"
-    addr_list: .word 0x20000000
-    max: .word 0x50000000
 
-.section .text
+.section .heap,"aw"
+	addr_list: .space 1024
+
+.text
 _start:
-	bl add_to_list
+	mov r0, #0
+	//setup initial block with size max
+	ldr r1, =addr_list
+	str r0, [r1]
+	mov r0, #1024
+	str r0, [r1, #4]
 	bl exit
 
 /*metadata is as follows
@@ -17,11 +23,23 @@ _start:
 
 add_to_list:
 	//address of block in r0
-	push {r4-r11}
+	//push {r4-r11}
 	//for simplicity, assume always one valid block
 	ldr r1, =addr_list
 	ldr r1, [r1]
-	pop {r4-r11}
+	mov r2, #0 //previous pointer
+	while_add_to_list:
+		cmp r1, #0
+		beq end_while_add_to_list
+		cmp r1, r0
+		bpl end_while_add_to_list
+		mov r2, r1
+		ldr r1, [r1]
+		b while_add_to_list
+
+	end_while_add_to_list:
+	str r0, [r2]
+	str r1, [r0]
 	bx lr
 
 
